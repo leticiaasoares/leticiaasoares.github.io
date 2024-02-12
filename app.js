88c33ce6bf444a6eee03cc04d6b54b5d59a27d9b
leticiaasoares <leticiaoliveiras2123@gmail.com>
2024-02-12 03:24:13 -0300

    teste

--- app.js
+++ app.js
@@ -1,29 +1,85 @@
-const menuLinks = document.querySelector('.barraNav__menu');
-const navLogo = document.querySelector('barraNav__logo');
+//lógia para mudar o tema entre modo escuro e modo claro
+function pegaTema() {
+    if (localStorage.getItem('tema')) { //verifica se ja tem informação no localStorage 
+        return localStorage.getItem('tema'); //usa o tema salvo no localStorage
+    }
+    else if (matchMedia('(prefers-color-scheme: dark)').matches) { //usa tema usado no browser
+        return 'dark';
+    }
+    else if(matchMedia('(prefers-color-scheme: light)').matches) {
+        return 'light';        
+    } else {
+        return 'light';
+    }
+}
+
+function salvaTema(tema) { //salva tema usado pelo usuario no localStorage
+    localStorage.setItem('tema', tema);
+}
+
+function aplicaTema(tema) { 
+    document.documentElement.dataset.theme = tema; //muda o tema no site
+    if(tema === 'light') { //muda o icon de sol ou lua
+        botaoTema.checked = false;
+        iconTema.setAttribute('name', 'sunny-outline');
+    } else {
+        botaoTema.checked = true;
+        iconTema.setAttribute('name', 'moon-outline');
+    }
+}
+
+function alternaTema(tema) { //logica usada para mudar tema pelo botão
+    if(tema == 'light') {
+        return 'dark';
+    }
+    return 'light';
+}
+
+let iconTema = document.querySelector('.iconmodo'); //icon de sol ou lua
+const botaoTema = document.getElementById('theme-toggle'); //botao de slide
+
+let tema = pegaTema(); //pega o tema que vai ser usado pela primeira vez
+aplicaTema(tema); //aplica esse tema no site
+
+botaoTema.onclick = () => { //muda o tema sempre que clicar no botao
+    const temaNovo = alternaTema(tema);
+    aplicaTema(temaNovo);
+    salvaTema(temaNovo);
+    tema = temaNovo;
+}
+
+
 
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
-    const motivoMenu = document.querySelector('#motivo-page');
-    const expectativasMenu = document.querySelector('#expectativas-page');
+    const sobreMenu = document.querySelector('#sobre-page');
+    const motivoMenu = document.querySelector('#motivos-page');
+    const expectativasMenu = document.querySelector('#expectativas-page')
     let scrollPos = window.scrollY;
 
-    if(window.innerWidth > 960 && scrollPos < 600) {
+    if(scrollPos < 600) {
         homeMenu.classList.add('highlight');
+        sobreMenu.classList.remove('highlight');
+        return
+    } else if(scrollPos < 1400) {
+        homeMenu.classList.remove('highlight');
+        sobreMenu.classList.add('highlight');
         motivoMenu.classList.remove('highlight');
         return
-    } else if(window.innerWidth > 960 && scrollPos < 1400) {
-        homeMenu.classList.remove('highlight');
+    } else if(scrollPos < 2200) {
+        sobreMenu.classList.remove('highlight');
         motivoMenu.classList.add('highlight');
         expectativasMenu.classList.remove('highlight');
         return
-    } else if(window.innerWidth > 960 && scrollPos < 2345) {
+    } else if(scrollPos < 3000) {
+        sobreMenu.classList.remove('highlight');
         motivoMenu.classList.remove('highlight');
         expectativasMenu.classList.add('highlight');
         return
     }
 
-    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    if ((scrollPos < 600) || elem) {
         elem.classList.remove('highlight');
     }
 };
@@ -31,7 +87,17 @@ const highlightMenu = () => {
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
 
-let icon = document.querySelector('ion-icon');
-icon.onclick = function(){
-  icon.classList.toggle('active');
+
+
+let iconSobreCoracao = document.querySelector('.sobre__iconcoracao');
+let iconMotivosCoracao = document.querySelector('.motivos__iconcoracao');
+let iconExpectativasCoracao = document.querySelector('.expectativas__iconcoracao');
+iconSobreCoracao.onclick = function() {
+    iconSobreCoracao.classList.toggle('active');
+}
+iconMotivosCoracao.onclick = function() {
+    iconMotivosCoracao.classList.toggle('active');
+}
+iconExpectativasCoracao.onclick = function() {
+    iconExpectativasCoracao.classList.toggle('active');
 }
